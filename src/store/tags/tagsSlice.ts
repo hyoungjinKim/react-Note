b7121e75cc848963f8ddab5b6253f04a09091a76
HYJIN\isacc <isacc7224@gmail.com>
2025-01-17 02:13:02 +0900

    commit: 로컬스토리지 저장 구현

--- src/store/tags/tagsSlice.ts
+++ src/store/tags/tagsSlice.ts
@@ -1,33 +1,51 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
-import { v4 } from 'uuid';
+import { v4 } from "uuid";
 
-const initialState = {
-    tagsList: [
-        { tag: "coding", id: v4() },
-        { tag: "exercise", id: v4() },
-        { tag: "quotes", id: v4() }
-    ]
+interface Tag {
+  tag: string;
+  id: string;
 }
 
+const getStoredTags = (): Tag[] => {
+  const storedData = localStorage.getItem("tagsList");
+  return storedData
+    ? JSON.parse(storedData)
+    : [
+        { tag: "coding", id: v4() },
+        { tag: "exercise", id: v4() },
+        { tag: "quotes", id: v4() },
+      ];
+};
+
+const saveToLocalStorage = (tagsList: Tag[]) => {
+  localStorage.setItem("tagsList", JSON.stringify(tagsList));
+};
+
+const initialState: { tagsList: Tag[] } = {
+  tagsList: getStoredTags(),
+};
+
 const tagsSlice = createSlice({
-    name: "tags",
-    initialState,
-    reducers: {
-        addTags: (state, { payload }) => {
-            if (state.tagsList.find(({ tag }) => tag === payload.tag)) {
-                toast.warning("이미 존재하는 태그입니다.");
-            } else {
-                state.tagsList.push(payload);
-                toast.info("새로운 태그가 등록되었습니다.");
-            }
-        },
-        deleteTags: (state, { payload }) => {
-            state.tagsList = state.tagsList.filter(({ id }) => id !== payload)
-            toast.info("태그가 삭제되었습니다.");
-        }
-    }
-})
+  name: "tags",
+  initialState,
+  reducers: {
+    addTags: (state, { payload }: { payload: Tag }) => {
+      if (state.tagsList.find(({ tag }: Tag) => tag === payload.tag)) {
+        toast.warning("이미 존재하는 태그입니다.");
+      } else {
+        state.tagsList.push(payload);
+        saveToLocalStorage(state.tagsList);
+        toast.info("새로운 태그가 등록되었습니다.");
+      }
+    },
+    deleteTags: (state, { payload }: { payload: string }) => {
+      state.tagsList = state.tagsList.filter(({ id }: Tag) => id !== payload);
+      saveToLocalStorage(state.tagsList);
+      toast.info("태그가 삭제되었습니다.");
+    },
+  },
+});
 
 export const { addTags, deleteTags } = tagsSlice.actions;
 
