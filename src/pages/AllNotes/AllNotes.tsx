8f37740fd0134e864d6ddc64f9561e426b60230b
HYJIN\isacc <isacc7224@gmail.com>
2025-01-17 09:02:52 +0900

    no message

--- src/pages/AllNotes/AllNotes.tsx
+++ src/pages/AllNotes/AllNotes.tsx
@@ -1,25 +1,24 @@
-import React, { useState } from 'react'
-import { FiltersModal } from '../../components';
-import { useAppDispatch, useAppSelector } from '../../hooks/redux'
-import { toggleFiltersModal } from '../../store/modal/modalSlice';
-import { ButtonOutline, Container, EmptyMsgBox } from '../../styles/styles';
-import getAllNotes from '../../utils/getAllNotes';
-import { Box, InputBox, TopBox } from './AllNotes.styles';
+import React, { useState } from "react";
+import { FiltersModal } from "../../components";
+import { useAppDispatch, useAppSelector } from "../../hooks/redux";
+import { toggleFiltersModal } from "../../store/modal/modalSlice";
+import { ButtonOutline, Container, EmptyMsgBox } from "../../styles/styles";
+import getAllNotes from "../../utils/getAllNotes";
+import { Box, InputBox, TopBox } from "./AllNotes.styles";
 
 const AllNotes = () => {
   const dispatch = useAppDispatch();
   const { mainNotes } = useAppSelector((state) => state.notesList);
   const { viewFiltersModal } = useAppSelector((state) => state.modal);
-  const [filter, setFilter] = useState('');
-  const [searchInput, setSearchInput] = useState('');
+  const [filter, setFilter] = useState("");
 
   const filterHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFilter(e.target.value);
-  }
+  };
 
   const clearHandler = () => {
     setFilter("");
-  }
+  };
 
   return (
     <Container>
@@ -32,22 +31,11 @@ const AllNotes = () => {
       )}
 
       {mainNotes.length === 0 ? (
-        <EmptyMsgBox>
-          노트가 없습니다.
-        </EmptyMsgBox>
+        <EmptyMsgBox>노트가 없습니다.</EmptyMsgBox>
       ) : (
         <>
           <TopBox>
-            <InputBox>
-              <input
-                type={"text"}
-                value={searchInput}
-                onChange={(e) => setSearchInput(e.target.value)}
-                placeholder="노트의 제목을 입력해주세요."
-              />
-            </InputBox>
-
-            <div className='notes__filter-btn'>
+            <div className="notes__filter-btn">
               <ButtonOutline
                 onClick={() => dispatch(toggleFiltersModal(true))}
                 className="nav__btn"
@@ -64,7 +52,7 @@ const AllNotes = () => {
         </>
       )}
     </Container>
-  )
-}
+  );
+};
 
-export default AllNotes
+export default AllNotes;
